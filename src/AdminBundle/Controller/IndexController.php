e1d74459fab137d47e496662a282bda9b2d4e5bf
amolk <amolk@alohatechnology.com>
2015-04-23 18:33:09 +0530

    test cegdeam 2

--- src/AdminBundle/Controller/IndexController.php
+++ src/AdminBundle/Controller/IndexController.php
@@ -13,27 +13,21 @@ class IndexController extends Controller
 {
     public function indexAction()
     {
-
     	$s_username	= '';
 		$s_password	= '';
     	$session 	= $this->getRequest()->getSession();
         $request 	= $this->get('request');
         if ($request->getMethod() == 'POST') {
-
         	$s_username	= trim($request->request->get('txt_username'));
 			$s_password	= trim($request->request->get('txt_password'));
-
 			$user = new User();
 			$user->setUsername($s_username);
-
 			if($s_password != '') {
 				$s_plain_password 	= $s_password;
 				$encoder 			= $this->container->get('security.password_encoder');
 				$s_password 		= $encoder->encodePassword($user, $s_plain_password);
 			}
-			
 			$user->setPassword($s_password);
-
 			$a_error_list   = $this->get('validator')->validate($user);
 			$s_error_msg    = "";
 			if (count($a_error_list) > 0) {
@@ -42,38 +36,28 @@ class IndexController extends Controller
 					break;
 				}
 			}
-
 			if($s_error_msg != '') {
 				return $this->render('AdminBundle:Index:index.html.twig',array('s_error' => $s_error_msg,'s_username' => $s_username));
 			} else {
 				$a_parameters = array('username' 	=> $s_username,
 								  	  'password'	=> $s_password);
-
 				$doctrine 	= $this->getDoctrine()->getManager();
 				$user 		= $doctrine->getRepository('MainBundle\Entity\User')->findOneBy($a_parameters);
-
 				if($user) {
-
-					// $s_expiration_date 	= date('Y-m-d',strtotime($user->getExpirationDate()->format('Y-m-d')));
-					// $s_current_date 	= date('Y-m-d');
-
-					// if($s_expiration_date != '' && $s_expiration_date <= $s_current_date) {
-
-					// 	return $this->render('AdminBundle:Index:index.html.twig',array('s_error' => 'Your account is expired','s_username' => $s_username));
-
-					// } else {
-
+					$s_expiration_date 	= date('Y-m-d',strtotime($user->getExpirationDate()->format('Y-m-d')));
+					$s_current_date 	= date('Y-m-d');
+					if($s_expiration_date != '' && $s_expiration_date <= $s_current_date) {
+						return $this->render('AdminBundle:Index:index.html.twig',array('s_error' => 'Your account is expired','s_username' => $s_username));
+					} else {
 						//clear previous session
 						$session->clear();
-
 						// create the session
 						$session = new Session();
 						$session->set('id',$user->getId());
 						$session->set('first_name',$user->getFirstName());
 						$session->set('last_name',$user->getLastName());
-
 						return $this->redirectToRoute('therapeutic_area_user');				
-					// }	
+					}
 				} else {
 					return $this->render('AdminBundle:Index:index.html.twig',array('s_error' => 'Invalid Credentials','s_username' => $s_username));
 				}
--- src/AdminBundle/Controller/TherapeuticAreaController.php
+++ src/AdminBundle/Controller/TherapeuticAreaController.php
@@ -12,6 +12,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\Config\Definition\Exception\Exception;
 
 
 class TherapeuticAreaController extends Controller
@@ -19,7 +20,6 @@ class TherapeuticAreaController extends Controller
 
 	public function indexAction()
 	{
-
 		$session = $this->getRequest()->getSession();
 		if(!$session->has('id')) {
 			return $this->redirectToRoute('admin');
@@ -30,52 +30,37 @@ class TherapeuticAreaController extends Controller
 	public function addEditAction(Request $request)
 	{
 		try {
-
 			$i_therapeutic_area_id = trim($request->request->get('hid_therapeutic_area_id'));
 			$doctrine = $this->getDoctrine()->getManager();
-
 			if($i_therapeutic_area_id > 0) {
-
 				// edit therapeutic area
 				$s_therapeutic_area_name = trim($request->request->get('txt_edit_therapeutic_area'));
-
 				$therapeutic_area   = $doctrine->getRepository('MainBundle\Entity\TherapeuticArea')->find($i_therapeutic_area_id);
 				$therapeutic_area->setName($s_therapeutic_area_name);
-
 				$a_error_list   = $this->get('validator')->validate($therapeutic_area);
-
 				$s_error_msg    = "";
 				if (count($a_error_list) > 0) {
 					foreach ($a_error_list as $err) {
 						$s_error_msg.= $err->getMessage();
 					}
 				}
-
 				if($s_error_msg != '') {
-
 					$a_response['s_status'] = 'error';
 					$a_response['data']     = $s_error_msg;
-
 				} else {
-
 					//update in database
 					$therapeutic_area   = $doctrine->getRepository('MainBundle\Entity\TherapeuticArea')->find($i_therapeutic_area_id);
 					$therapeutic_area->setName($s_therapeutic_area_name);
 					$doctrine->persist($therapeutic_area);
 					$doctrine->flush();
-
 					$a_response['s_status'] = 'success';
 					$a_response['data']     = '';
-						
 				}   
 			} else {
-				
 				// add therapeutic area
 				$s_therapeutic_area_name = trim($request->request->get('txt_therapeutic_area'));
-
 				$therapeutic_area = new TherapeuticArea();
 				$therapeutic_area->setName($s_therapeutic_area_name);
-
 				$a_error_list   = $this->get('validator')->validate($therapeutic_area);
 				$s_error_msg    = "";
 				if (count($a_error_list) > 0) {
@@ -83,59 +68,40 @@ class TherapeuticAreaController extends Controller
 						$s_error_msg.= $err->getMessage() . "\n";
 					}
 				}
-
 				if($s_error_msg != '') {
-
 					$a_response['s_status'] = 'error';
 					$a_response['data']     = $s_error_msg;
-
 				} else {
-
 					// add new in database
 					$therapeutic_area = new TherapeuticArea();
 					$therapeutic_area->setName($s_therapeutic_area_name);
 					$doctrine->persist($therapeutic_area);
 					$doctrine->flush();
-
 					$a_response['s_status'] = 'success';
 					$a_response['data']     = '';
-					
 				}
 			}   
 
 		} catch(Exception $e) {
-
 			$a_response['s_status'] = 'error';
 			$a_response['data']     = $e->getMessage();
-
 		}
-
 		return new JsonResponse($a_response);
-
 	}
 
 	public function getListAction()
 	{
-		try {   
-
+		try {
 			$repository = $this->getDoctrine()->getRepository('MainBundle\Entity\TherapeuticArea');
 			$therapeutic_area = $repository->findBy(array(),array('name' => 'ASC'));
-
 			$template = $this->renderView('AdminBundle:TherapeuticArea:list.html.twig',array('therapeutic_area'=> $therapeutic_area));
-
 			$a_response['s_status']     = 'success';
 			$a_response['data']         = $template;
 			$a_response['i_record']     = count($therapeutic_area);
-
 		} catch(Exception $e) {
-
 			$a_response['s_status'] = 'error';
 			$a_response['data']     = $e->getMessage();
-
 		}
-
 		return new JsonResponse($a_response);
-
 	}
-
 }
